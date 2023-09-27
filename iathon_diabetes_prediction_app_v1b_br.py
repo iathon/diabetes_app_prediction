b5dd61ba9af8b256458f61dc916bdb0d5539f2f7
Nuno Machado <nuno.machado.br@gmail.com>
2023-09-27 20:13:48 +0100

    simplified app

--- iathon_diabetes_prediction_app_v1b_br.py
+++ iathon_diabetes_prediction_app_v1b_br.py
@@ -1,80 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# import streamlit as st
-# import pandas as pd
-# import joblib
-# from sklearn.preprocessing import LabelEncoder
-# 
-# # Load the trained Random Forest model
-# model = joblib.load('diabetes_prediction_sylhetdb_rf_model.pkl')
-# 
-# # Define a dictionary to map gender values to 0 (male) and 1 (female)
-# gender_mapping = {'Masculino': 0, 'Feminino': 1}
-# 
-# # Function to preprocess user input data
-# def preprocess_input(idade, genero, poliuria, polidipsia, perda_peso_repentina, polifagia, visao_embacada, irritabilidade, cicatrizacao_lenta, alopecia, obesidade):
-#     # Encode gender
-#     genero_codificado = gender_mapping[genero]
-# 
-#     # Create a DataFrame with the user input
-#     user_data = pd.DataFrame({
-#         'idade': [idade],
-#         'genero': [genero_codificado],
-#         'poliuria': [poliuria],
-#         'polidipsia': [polidipsia],
-#         'perda_peso_repentina': [perda_peso_repentina],
-#         'polifagia': [polifagia],
-#         'visao_embacada': [visao_embacada],
-#         'irritabilidade': [irritabilidade],
-#         'cicatrizacao_lenta': [cicatrizacao_lenta],
-#         'alopecia': [alopecia],
-#         'obesidade': [obesidade]
-#     })
-# 
-#     return user_data
-# 
-# # Streamlit app
-# def main():
-#     st.title("Aplicativo de Previsão de Diabetes")
-# 
-#     # User input form
-#     st.header("Informações do Usuário")
-#     idade = st.slider("Idade", 0, 100, 30)
-#     genero = st.selectbox("Gênero", ("Masculino", "Feminino"))
-#     poliuria = st.checkbox("Poliúria")
-#     polidipsia = st.checkbox("Polidipsia")
-#     perda_peso_repentina = st.checkbox("Perda de Peso Repentina")
-#     polifagia = st.checkbox("Polifagia")
-#     visao_embacada = st.checkbox("Visão Embaçada")
-#     irritabilidade = st.checkbox("Irritabilidade")
-#     cicatrizacao_lenta = st.checkbox("Cicatrização Lenta")
-#     alopecia = st.checkbox("Alopecia")
-#     obesidade = st.checkbox("Obesidade")
-# 
-#     if st.button("Prever"):
-#         # Preprocess user input
-#         user_data = preprocess_input(idade, genero, poliuria, polidipsia, perda_peso_repentina, polifagia, visao_embacada, irritabilidade, cicatrizacao_lenta, alopecia, obesidade)
-# 
-#         # Make a prediction
-#         prediction = model.predict_proba(user_data)[:, 1]
-# 
-#         st.header("Resultado da Previsão")
-#         st.write(f"Probabilidade de desenvolver diabetes: {prediction[0]:.2f}")
-# 
-# if __name__ == '__main__':
-#     main()
-# 
-
-# In[1]:
-
-
 import streamlit as st
 import pandas as pd
 import joblib
 from sklearn.preprocessing import LabelEncoder
 
-# Load the trained Random Forest model
+# Load the trained LightGBM model
 model = joblib.load('diabetes_prediction_sylhetdb_rf_model.pkl')
 
 # Function to preprocess user input data
@@ -103,21 +32,17 @@ def preprocess_input(idade, poliuria, polidipsia, perda_peso_repentina, polifagi
 
 # Streamlit app
 def main():
-    st.title("Teste a sua probabilidade para desenvolver Diabetes")
-    st.write("Com alguns poucos dados seus, o nosso modelo de IA vai calcular as suas chances de ter diabetes.")
-    st.write("Experimente e tranquilize-se!")
-
+    
     # User input form
-    st.header("Informações do Usuário")
     idade = st.slider("Idade", 0, 100, 30)
-    poliuria = st.checkbox("Poliúria", help="Produção excessiva de urina")
-    polidipsia = st.checkbox("Polidipsia", help="Sede excessiva")
-    perda_peso_repentina = st.checkbox("Perda de Peso Repentina", help="Perda significativa de peso sem motivo aparente")
-    polifagia = st.checkbox("Polifagia", help="Aumento anormal do apetite")
-    visao_embacada = st.checkbox("Visão Embaçada", help="Visão embaçada ou turva")
-    irritabilidade = st.checkbox("Irritabilidade", help="Alterações frequentes de humor")
-    cicatrizacao_lenta = st.checkbox("Cicatrização Lenta", help="Feridas que demoram a cicatrizar")
-    alopecia = st.checkbox("Alopecia", help="Perda de cabelo")
+    poliuria = st.checkbox("Produção excessiva de urina", help="Poliúria")
+    polidipsia = st.checkbox("Sede excessiva", help="Polidipsia")
+    perda_peso_repentina = st.checkbox("Perda significativa de peso sem motivo aparente", help="Perda de Peso Repentina")
+    polifagia = st.checkbox("Aumento anormal do apetite", help="Polifagia")
+    visao_embacada = st.checkbox("Visão embaçada ou turva", help="Visão Embaçada")
+    irritabilidade = st.checkbox("Alterações frequentes de humor", help="Irritabilidade")
+    cicatrizacao_lenta = st.checkbox("Feridas que demoram a cicatrizar", help="Cicatrização Lenta")
+    alopecia = st.checkbox("Perda de cabelo", help="Alopecia")
     peso = st.number_input("Peso (kg)", 0.0)
     altura = st.number_input("Altura (cm)", 0.0)
 
